e5ef268389e0fb02bf3d06ddb06e1fa82060e87e
Steph St <steph.streete@outlook.com>
2020-10-03 14:00:53 +0000

    Added more code

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 #Write a program to ask a student their percentage mark and convert this to a grade - Conversion will be in a function called mark_grade
 # ask the user for their target grade and print this with their mark
 print("Grades")
-mark_grade = 0 
+
+mark_grade = ("A,B")
+
 number = int(input("What percentage did you get?"))
 
-if (number > 75):
+if (number > 50):
   print ("Your grade is A")
-if (number < 75):
+if (number < 50):
   print ("Your grade is B")
+
+target = input("What was your target grade?")
+
+while (mark_grade == "A"):
+  print("Well done you met your target")
+if mark_grade <= B :
+  print ("Your grade was less than your target - please rebook your retest")
+elif mark_grade >= A:
+  print("Well done you went above and beyond!")
+
