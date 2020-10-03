9b3458a7b4bfc6c3c6109f26317c57d680129c52
Steph St <steph.streete@outlook.com>
2020-10-03 11:04:54 +0000

    Added question as comment

--- main.py
+++ main.py
@@ -1,1 +1,2 @@
-#Write a program to ask a student their percentage mark and convert this to a grade
+#Write a program to ask a student their percentage mark and convert this to a grade - Conversion will be in a function called mark_grade
+# ask the user for their target grade and print this with their mark
